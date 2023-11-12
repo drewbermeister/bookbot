30bc32c5f2002c73c6918293844f1f6a5a5d968e
drewbermeister <abfarin@gmail.com>
2023-11-12 09:53:58 -0800

    An attempt at the first couple exercises. Not able to get it to run for some reason.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-print("hello world")
+def read_book():
+    with open("/github.com/drewbermeister/bookbot/books/frankenstein.txt") as f:
+        file_context = f.read()
+        print(file_context)
 
-# TODO
-# Section 13 - Bookbot - Read File
+        count_words(file_context)
+        f.close()
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    print("hello world!")
+    #read_book()
+
